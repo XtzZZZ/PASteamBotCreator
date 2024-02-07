6496c55f2f4ed97b036ec2b441be4d6767d9bbe5
XtzZZZ <maxim.savochkin120@gmail.com>
2024-02-07 22:10:09 +0100

    .

--- PASteamBotCreator/Scraper.cs
+++ PASteamBotCreator/Scraper.cs
@@ -34,19 +34,21 @@ public class Scraper
             driver.Quit();
         }
     }
-    
+    // /html/body/div[1]/div[7]/div[6]/div/div[1]/div[2]/form/div/div/div[4]/div[1]/div/select/option[14]
     private void FirstPage()
     {
         driver.Navigate().GoToUrl(url: url);
         Thread.Sleep(5000);
-        IWebElement EmailInput = driver.FindElement(By.Id("email"));
+        driver.FindElement(By.CssSelector("#email")).SendKeys("otobasgumats@gmail.com");
+        driver.FindElement(By.CssSelector("#reenter_email")).SendKeys("otobasgumats@gmail.com");
+        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+        js.ExecuteScript("document.getElementById('g-recaptcha-response').removeAttribute('disabled')");
+        Console.WriteLine("Done");
+        Thread.Sleep(500000);
+        driver.FindElement(By.CssSelector("#country")).Click();
         Thread.Sleep(5000);
-        EmailInput.SendKeys("kurwa@bober.pl");
+        driver.FindElement(By.XPath("//*[@id=\"country\"]/option[8]")).Click();
         Thread.Sleep(5000);
-        using (FileStream fs = new FileStream("test.txt", FileMode.OpenOrCreate))
-        {
-            byte[] buffer = Encoding.Default.GetBytes(driver.PageSource);
-            fs.Write(buffer);
-        } //
+        
     }
 }
